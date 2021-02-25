c401cc611c362f98a21a81a92ae47b4c3c760914
ChanGrea <k39335@naver.com>
2021-02-26 08:15:06 +0900

    getData() 테스트 코드 추가

--- src/js/game.test.js
+++ src/js/game.test.js
@@ -1,7 +1,16 @@
-const { getScore } = require("./game");
+const { getScore, getData } = require("./game");
 
 test("Get Score", () => {
   let curScore = 0;
   expect(getScore(curScore, 1)).toBe(1);
   expect(getScore(curScore, -1)).toBe(-1);
+});
+
+test("Get Data From Remote", () => {
+  let response = null;
+  getData().then(result => {
+    response = result;
+    expect(response).toEqual(expect.any(Array));
+    expect(response.length > 0).toBe(true);
+  });
 });
